ed6636b8148247cd35a163d3c2cc70c99ca4739e
Imre-Vaczi <imre.vaczi@gmail.com>
2025-05-29 18:21:07 +0200

    pimping listazas method to tag data

--- JegyFoglalasKezelo.py
+++ JegyFoglalasKezelo.py
@@ -24,7 +24,7 @@ class JegyFoglalasKezelo:
     def jegy_listazas(self, utas_nev):
         for jegy in self.jegyek:
             if not jegy.get_status() and jegy.get_utas() == utas_nev:
-                print("Utas:",jegy.get_utas(),"| ", jegy.get_foglalasID(),"| Dátum: ", jegy.get_jaratdatum()," | Célállomás: ", jegy.jarat_indulas.get_cel())
+                print("Utas:",jegy.get_utas(),"| Foglalási azonosító: ", jegy.get_foglalasID(),"| Dátum: ", jegy.get_jaratdatum()," | Célállomás: ", jegy.jarat_indulas.get_cel())
 
     def jegy_kereses(self, id):
         for jegy in self.jegyek:
